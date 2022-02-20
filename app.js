d3148a8093694e68dded1e0523b94ca08c2babc9
Md akter hosen <mdaktarhossen20@gmail.com>
2022-02-20 09:57:52 +0600

    funcation decleare has completed

--- app.js
+++ app.js
@@ -1,68 +1,35 @@
-// get mobile pluse button
-document.getElementById("mobile-plus-btn").addEventListener("click", ()=>{
-    const mobileInpute=document.getElementById("mobile-number")
-    const mobileNumber=parseInt(mobileInpute.value);
-
-    const mobilePriceTag=document.getElementById("mobile-price")
-    const mobilePrice=parseFloat(mobilePriceTag.innerText);
-
-    if(mobileNumber==10){
-      alert("sorry enought number");
-    }
-    else{
-      const aktar=mobileInpute.value=mobileNumber+1;
-      mobilePriceTag.innerText=aktar*1219;
-    };
-});
-
-// get mobile minus button
-document.getElementById("mobile-minus-btn").addEventListener("click", ()=>{
+function updateMobileNumber(isIncrement){
   const mobileInpute=document.getElementById("mobile-number")
   const mobileNumber=parseInt(mobileInpute.value);
 
-  const mobilePriceTag=document.getElementById("mobile-price")
-  const mobilePrice=parseFloat(mobilePriceTag.innerText);
-  if(mobileNumber>0){
-    const aktar=mobileInpute.value=mobileNumber-1;
-      mobilePriceTag.innerText=mobilePrice-1219;
+  if(isIncrement){
+    mobileInpute.value=mobileNumber+1;
   }
-  else{
-    alert("0 Not count")
+  else if(mobileNumber>0){
+    mobileInpute.value=mobileNumber-1;
   }
-  
+}
+
+document.getElementById("mobile-plus-btn").addEventListener("click", ()=>{
+  updateMobileNumber(true)
 })
+
+document.getElementById("mobile-minus-btn").addEventListener("click", ()=>{
+  updateMobileNumber(false)
+})
+
 
 // get cover pluse button
 document.getElementById("cover-plus-btn").addEventListener("click", ()=>{
     const coverInpute=document.getElementById("cover-number")
     const coverNumber=parseInt(coverInpute.value);
-
-    const coverPriceTag=document.getElementById("cover-price")
-    const coverPrice=parseFloat(coverPriceTag.innerText);
-    if(coverNumber==10){
-      alert("sorry enought number")
-    }
-    else{
-     const aktar= coverInpute.value=coverNumber+1;
-      coverPriceTag.innerText=aktar*59;
-    }
+    coverInpute.value=coverNumber+1
+    
 });
 // get cover minus button
 document.getElementById("cover-minus-btn").addEventListener("click", ()=>{
   const coverInpute=document.getElementById("cover-number")
   const coverNumber=parseInt(coverInpute.value);
-
-  const coverPriceTag=document.getElementById("cover-price")
-  const coverPrice=parseFloat(coverPriceTag.innerText);
-    if(coverNumber==0){
-      alert("sorry enought number")
-    }
-    else if(coverNumber==10){
-      alert("11 not count")
-    }
-    else{
-      coverInpute.value=coverNumber-1;
-      coverPriceTag.innerText=coverPrice-59;
-    }
+  coverInpute.value=coverNumber-1
   
 })

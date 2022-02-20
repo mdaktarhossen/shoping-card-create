e2a98cb24c0ba4039e51b0ea2f26d3dde3cfe7dd
Md akter hosen <mdaktarhossen20@gmail.com>
2022-02-20 16:24:20 +0600

    shoping card is done

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ function updateMobileNumber(prodictName,price,isIncrement){
     const mobileNumber=getInpute.value=getNumber+1;
     const mobilePrice=document.getElementById(prodictName+"-price");
     mobilePrice.innerText=mobileNumber*price;
+    calculate()
   }
   
   else if(getNumber>0){
     const mobileNumber= getInpute.value=getNumber-1;
     const mobilePrice=document.getElementById(prodictName+"-price")
     mobilePrice.innerText=parseInt(mobilePrice.innerText)-price;
+    calculate()
   };
+};
+
+function getInputeValue(product){
+  const mobileInpute=document.getElementById(product +"-number")
+  const mobileNumber=mobileInpute.value;
+  return mobileNumber;
+};
+
+function calculate(){
+  const totalPhonePrice=getInputeValue("mobile")*1219;
+  const totalCoverPrice=getInputeValue("cover")*59;
+  const subTotal=totalPhonePrice+totalCoverPrice;
+  const tax=subTotal/10;
+  const total=subTotal+tax;
+
+  document.getElementById("sub-total").innerText=subTotal;
+  document.getElementById("tax").innerText=tax;
+  document.getElementById("total").innerText=total;
 };
 
 // calling updated mobile number and price;
@@ -34,4 +54,4 @@ updateMobileNumber( "cover",59,true)
 // get cover minus button;
 document.getElementById("cover-minus-btn").addEventListener("click", ()=>{
   updateMobileNumber( "cover",59,false)
-})
+});

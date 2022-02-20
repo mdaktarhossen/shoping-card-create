6f844e28ebaf9afda5d584ae557b276c3bb81def
Md akter hosen <mdaktarhossen20@gmail.com>
2022-02-20 12:26:56 +0600

    funcation

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 function updateMobileNumber(isIncrement){
-  const mobileInpute=document.getElementById("mobile-number")
-  const mobileNumber=parseInt(mobileInpute.value);
+  const getInpute=document.getElementById("mobile-number");
+  const getNumber=parseInt(getInpute.value);
+  // return getNumber;
 
   if(isIncrement){
-    mobileInpute.value=mobileNumber+1;
+    const mobileNumber=getInpute.value=getNumber+1;
+
+    const mobilePrice=document.getElementById("mobile-price");
+    mobilePrice.innerText=mobileNumber*1219;
   }
-  else if(mobileNumber>0){
-    mobileInpute.value=mobileNumber-1;
-  }
+  else if(getNumber>0){
+    const mobileNumber= getInpute.value=getNumber-1;
+
+    const mobilePrice=document.getElementById("mobile-price")
+    mobilePrice.innerText=parseInt(mobilePrice.innerText)-1219;
+  };
 }
 
+// calling updated mobile number and price 
 document.getElementById("mobile-plus-btn").addEventListener("click", ()=>{
-  updateMobileNumber(true)
+  updateMobileNumber(true);
 })
 
 document.getElementById("mobile-minus-btn").addEventListener("click", ()=>{
-  updateMobileNumber(false)
+  updateMobileNumber(false);
 })
-
 
 // get cover pluse button
 document.getElementById("cover-plus-btn").addEventListener("click", ()=>{
-    const coverInpute=document.getElementById("cover-number")
+    const coverInpute=document.getElementById("cover-number");
     const coverNumber=parseInt(coverInpute.value);
-    coverInpute.value=coverNumber+1
+    coverInpute.value=coverNumber+1;
     
 });
 // get cover minus button
 document.getElementById("cover-minus-btn").addEventListener("click", ()=>{
-  const coverInpute=document.getElementById("cover-number")
+  const coverInpute=document.getElementById("cover-number");
   const coverNumber=parseInt(coverInpute.value);
-  coverInpute.value=coverNumber-1
+  coverInpute.value=coverNumber-1;
   
 })
